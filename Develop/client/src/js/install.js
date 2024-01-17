92ad1c5854fc375dbfcd7a7451e0babb165cc6eb
sush-tech <sushmithar31@gmail.com>
2024-01-16 21:52:36 -0500

    pushing to heroku

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,7 +1,5 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     console.log('hit')
     console.log("event" + event)
@@ -13,8 +11,6 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     // console.log(promptEvent)
@@ -31,7 +27,6 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     // Clear prompt
     console.log('install hit')
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,15 +3,16 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -51,6 +52,7 @@ module.exports = () => {
     ],
 
     module: {
+      // CSS Loaders
       rules: [
         {
           test: /\.css$/i,
