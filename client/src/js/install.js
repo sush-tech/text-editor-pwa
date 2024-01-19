9b95061a574e0951c4c304f31b99ec6cbce57623
sush-tech <sushmithar31@gmail.com>
2024-01-18 21:50:31 -0500

    file structure

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,7 +3,6 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
     console.log('hit')
     console.log("event" + event)
-    event.preventDefault();
     // Store the triggered events
     window.deferredPrompt = event;
 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,9 +10,6 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      database: './src/js/database.js',
-      editor: './src/js/editor.js',
-      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
